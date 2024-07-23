f73428d49f6a84dd7bf42f12c6e0c186e7672c0a
Joaquin Esguerra <joaquin.esguerra@snhu.edu>
2024-07-23 11:22:31 -0400

    Module 4 completed baseline

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const hbs = require('hbs');
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
+var handlebars = require('hbs');
 
 
 var app = express();
@@ -15,7 +16,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 
-hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'))
+// register handlebars partial (https://www.npmjs.com/package/hbs)
+handlebars.registerPartials(__dirname + '/app_server/views/partials');
 
 app.set('view engine', 'hbs');
 

50042b73adc88946a963e044884532cb7c891f88
Joaquin Esguerra <joaquin.esguerra@snhu.edu>
2024-08-22 12:38:48 -0400

    mongoose connection fixes

--- app_api/models/travlr.js
+++ app_api/models/travlr.js
@@ -1,4 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/travlr', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(err => {
+    console.error('Error connecting to MongoDB:', err);
+  });
+
 const host = process.env.DB_HOST || '127.0.0.1';
 const dbURI = `mongodb://${host}/travlr`;
 const readLine = require('readline');
